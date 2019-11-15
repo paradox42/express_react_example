276601bd116f49342debfb739f676df20f6b432a
Paradox <jhe@nexient.com>
2019-11-15 12:45:03 -0500

    First commit after project set up. Will try figure out deployment flow up on next.

---
+++ server.js
@@ -0,0 +1,14 @@
+import express from "express";
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+
+// console.log that your server is up and running
+app.listen(port, () => console.log(`Listening on port ${port}`));
+
+// create a GET route
+app.get('/express_backend', (req, res) => {
+  res.send({ express: 'YOUR EXPRESS BACKEND IS CONNECTED TO REACT' });
+});
+
